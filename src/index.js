7a925360a88ae73c0876444591699f11b7fab721
Marcin Czaniecki <62254945+janossik@users.noreply.github.com>
2022-02-07 09:58:21 +0100

    first timeline

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import gsap from 'gsap';
 
-const root = document.getElementById('root');
+const tl = gsap.timeline({
+  defaults: {
+    duration: 2,
+    ease: 'power1.inOut',
+    yoyo: true,
+    repeat: -1,
+  },
+});
 
-root.innerHTML = `Project template for GSAP3 course`;
-
-// eslint-disable-next-line no-console
-console.log(gsap, root);
+tl.to('#cr1', { x: 500 })
+  .to('#cr2', { x: 461.94, y: -191.64 }, '<0.25')
+  .to('#cr3', { x: 353.55, y: -353.56 }, '<0.25')
+  .to('#cr4', { x: 191.34, y: -461.94 }, '<0.25')
+  .to('#cr5', { y: -500 }, '<0.25')
+  .to('#cr6', { x: -191.34, y: -461.94 }, '<0.25')
+  .to('#cr7', { x: -353.55, y: -353.56 }, '<0.25')
+  .to('#cr8', { x: -461.94, y: -191.64 }, '<0.25');
