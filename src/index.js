f9347c1f65154effa6a5f09190ff2ece55e9bba8
Marcin Czaniecki <62254945+janossik@users.noreply.github.com>
2022-02-07 07:58:44 +0100

    to, from , fromTo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 import gsap from 'gsap';
 
-const root = document.getElementById('root');
+const circleA = document.getElementById('a');
+const circleB = document.getElementById('b');
+const circleC = document.getElementById('c');
 
-root.innerHTML = `Project template for GSAP3 course`;
-
-// eslint-disable-next-line no-console
-console.log(gsap, root);
+gsap.to(circleA, {
+  duration: 3,
+  delay: 2,
+  x: 700,
+  repeat: -1,
+  ease: 'none',
+});
+gsap.from(circleB, {
+  duration: 3,
+  delay: 2,
+  x: 700,
+  repeat: -1,
+  ease: 'none',
+});
+gsap.fromTo(
+  circleC,
+  {
+    x: 200,
+  },
+  {
+    duration: 3,
+    delay: 2,
+    x: 700,
+    repeat: -1,
+    ease: 'none',
+  },
+);
