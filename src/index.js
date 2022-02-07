1ed6f201575f959692efdc487596918ba7d827e8
Marcin Czaniecki <62254945+janossik@users.noreply.github.com>
2022-02-07 02:54:21 +0100

    init project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import gsap from 'gsap';
+
+const root = document.getElementById('root');
+
+root.innerHTML = `Project template for GSAP3 course`;
+
+// eslint-disable-next-line no-console
+console.log(gsap, root);
