97c7b618612365ace9c4b9b12950336f68d43644
Marcin Czaniecki <62254945+janossik@users.noreply.github.com>
2022-02-08 12:56:26 +0100

    party 1 staggered rotate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import gsap from 'gsap';
 
-const root = document.getElementById('root');
+const addImage = (i) => {
+  const img = document.createElement('img');
+  img.src = `images/${i}.png`;
+  img.style.position = 'absolute';
+  img.className = 'code';
+  document.getElementById('root').prepend(img);
+};
 
-root.innerHTML = `Project template for GSAP3 course`;
+for (let i = 1; i <= 22; i += 1) {
+  addImage(i);
+}
 
-// eslint-disable-next-line no-console
-console.log(gsap, root);
+let skip = 1;
+let increment = 2;
+let angleZ = 0;
+let angleY = 0;
+
+window.addEventListener('mousemove', () => {
+  if (skip % 5 === 0) {
+    if (angleY < -89) {
+      if (angleY % 90 === 0) {
+        increment *= -1;
+      }
+    }
+    gsap.to('.code', {
+      duration: 2,
+      rotateY: angleY,
+      rotateZ: -angleZ,
+      stagger: 0.1,
+    });
+    skip += 1;
+    angleY -= 5 * Math.abs(increment);
+    angleZ += increment;
+  }
+  skip += 1;
+});
