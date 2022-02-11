35f7c430248d6afa7677a8b93e65d5f6e3b23508
Marcin Czaniecki <62254945+janossik@users.noreply.github.com>
2022-02-11 11:59:46 +0100

    water animation in vanilla js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
 import gsap from 'gsap';
 
-gsap.to('#feTurbulence', {
+const element = document.getElementById('feTurbulence');
+let frequency = 0.02;
+let yoyo = false;
+
+const animation = () => {
+  if (frequency > 0.04) {
+    yoyo = true;
+  } else if (frequency < 0.02) {
+    yoyo = false;
+  }
+  frequency += yoyo ? -0.00005 : 0.00005;
+  element.setAttribute('baseFrequency', frequency);
+};
+
+const main = () => {
+  animation();
+  window.requestAnimationFrame(main);
+};
+
+window.requestAnimationFrame(main);
+
+/* gsap.to('#feTurbulence', {
   duration: 20,
   attr: { baseFrequency: 0.01 },
   repeat: -1,
   yoyo: true,
 });
+ */
